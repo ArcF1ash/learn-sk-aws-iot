0507320b14ee4123e1b1019cd99aa420df9534a4
Aaron <aaron@emeryautomation.ca>
2024-11-14 15:37:43 -0800

    Added websocket

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,28 @@
-import config from './config';
-import connectAndSubscribe from './client';
+//import config from './config';
+//import connectAndSubscribe from './client';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
 
-connectAndSubscribe(config.AWS_ENDPOINT,config.AWS_CERT_PATH,config.AWS_KEY_PATH, config.AWS_IOT_TOPIC);
+// Create an http server and wrap it with Socket.IO
+const httpServer = createServer();
+const io = new Server(httpServer, {
+    cors: { origin: '*'}
+});
+
+// When a client connects
+io.on('connection', (socket) => {
+    console.log('New client connected');
+
+    // When client disconnects
+    socket.on('disconnect', () => {
+        console.log('Client disconnected');
+    })
+})
+
+// Start the server
+const PORT = 3000;
+httpServer.listen(PORT, () => {
+    console.log('Server is running on port', PORT);
+})
+
+//connectAndSubscribe(config.AWS_ENDPOINT,config.AWS_CERT_PATH,config.AWS_KEY_PATH, config.AWS_IOT_TOPIC);
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,17 @@
+<script lang="ts">
+    import { onMount, onDestroy } from "svelte";
+    import { connectToSocketIO } from "$lib/ws/ws-client";
+
+    let messages: string[] = [];
+
+    onMount(() => {
+        const socket =connectToSocketIO((message) => {
+            messages = [message, ...messages];
+        });
+
+        onDestroy(() => socket.disconnect());
+    });
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
